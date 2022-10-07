1b597b9abb31aa37723960e6684f180b10b0e81c
Cub3333 <kafu014@gmail.com>
2022-10-07 15:27:11 +0900

    add sort func

--- main.c
+++ main.c
@@ -1,10 +1,30 @@
 #include<stdio.h>
 
+void sort(int * array, int len)
+{
+    int tmp;
+    for(int i = 0; i < len; i++){
+        for(int j = i+1; j < len; j++){
+            if(array[i] > array[j]){
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+    }
+}
 int
 main()
 {
     printf("Hello World!");
     printf("%d", 1+2);
+    
+    int array[5] = {5, 4, 3, 1, 2};
+    sort(array, 5);
+
+    for(int i = 0; i < 5; i++){
+        printf("%d ", array[i]);
+    }
 
     return 0;
 }
